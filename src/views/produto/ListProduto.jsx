a335a26e69639b2148c792cfc42263a9f5275ab0
Matinhas <matheusvictor007@gmail.com>
2024-08-21 12:58:53 -0300

    Implementação do Filtro na tela de Produtos

--- src/views/produto/ListProduto.jsx
+++ src/views/produto/ListProduto.jsx
@@ -6,11 +6,15 @@ import MenuSistema from '../../MenuSistema';
 
 export default function ListProduto() {
 
-    const [lista, setLista] = useState([]);
+    const [lista, setListaProduto] = useState([]);
     const [openModal, setOpenModal] = useState(false);
     const [idRemover, setIdRemover] = useState();
+    const [menuFiltro, setMenuFiltro] = useState();
+    const [codigo, setCodigo] = useState();
+    const [titulo, setTitulo] = useState();
     const [idCategoria, setIdCategoria] = useState();
     const [listaCategoriaProduto, setListaCategoriaProduto] = useState([]);
+
 
 
     useEffect(() => {
@@ -21,8 +25,22 @@ export default function ListProduto() {
 
         axios.get("http://localhost:8080/api/produto")
             .then((response) => {
-                setLista(response.data)
+                setListaProduto(response.data)
             })
+
+        axios.get("http://localhost:8080/api/categoriaproduto")
+            .then((response) => {
+
+                const dropDownCategorias = [];
+                dropDownCategorias.push({ text: '', value: '' });
+                response.data.map(c => (
+                    dropDownCategorias.push({ text: c.descricao, value: c.id })
+                ))
+
+                setListaCategoriaProduto(dropDownCategorias)
+
+            })
+
     }
 
     function confirmaRemover(id) {
@@ -48,6 +66,57 @@ export default function ListProduto() {
         setOpenModal(false)
     }
 
+    //Função para filtrar os produtos	
+
+    function handleMenuFiltro() {
+
+        if (menuFiltro === true) {
+            setMenuFiltro(false);
+        } else {
+            setMenuFiltro(true);
+        }
+    }
+
+    function handleChangeCodigo(value) {
+
+        filtrarProdutos(value, titulo, idCategoria);
+    }
+
+    function handleChangeTitulo(value) {
+
+        filtrarProdutos(codigo, value, idCategoria);
+    }
+
+    function handleChangeCategoriaProduto(value) {
+
+        filtrarProdutos(codigo, titulo, value);
+    }
+
+    async function filtrarProdutos(codigoParam, tituloParam, idCategoriaParam) {
+
+        let formData = new FormData();
+
+        if (codigoParam !== undefined) {
+            setCodigo(codigoParam)
+            formData.append('codigo', codigoParam);
+        }
+        if (tituloParam !== undefined) {
+            setTitulo(tituloParam)
+            formData.append('titulo', tituloParam);
+        }
+        if (idCategoriaParam !== undefined) {
+            setIdCategoria(idCategoriaParam)
+            formData.append('idCategoria', idCategoriaParam);
+        }
+
+        await axios.post("http://localhost:8080/api/produto/filtrar", formData)
+            .then((response) => {
+                setListaProdutos(response.data)
+            })
+    }
+
+
+
 
 
 
@@ -65,6 +134,18 @@ export default function ListProduto() {
                     <Divider />
 
                     <div style={{ marginTop: '4%' }}>
+
+                        <Menu compact>
+                            <Menu.Item
+                                name='menuFiltro'
+                                active={menuFiltro === true}
+                                onClick={() => handleMenuFiltro()}
+                            >
+                                <Icon name='filter' />
+                                Filtrar
+                            </Menu.Item>
+                        </Menu>
+
                         <Button
                             label='Novo'
                             circular
@@ -74,7 +155,50 @@ export default function ListProduto() {
                             as={Link}
                             to='/form-produto'
                         />
-                        <br /><br /><br />
+                        {menuFiltro ?
+
+                            <Segment>
+                                <Form className="form-filtros">
+
+                                    <Form.Input
+                                        icon="search"
+                                        value={codigo}
+                                        onChange={e => handleChangeCodigo(e.target.value)}
+                                        label='Código do Produto'
+                                        placeholder='Filtrar por Código do Produto'
+                                        labelPosition='left'
+                                        width={4}
+                                    />
+
+                                    <Form.Group widths='equal'>
+                                        <Form.Input
+                                            icon="search"
+                                            value={titulo}
+                                            onChange={e => handleChangeTitulo(e.target.value)}
+                                            label='Título'
+                                            placeholder='Filtrar por título'
+                                            labelPosition='left'
+                                        />
+                                        <Form.Select
+                                            placeholder='Filtrar por Categoria'
+                                            label='Categoria'
+                                            options={listaCategoriaProduto}
+                                            value={idCategoria}
+                                            onChange={(e, { value }) => {
+                                                handleChangeCategoriaProduto(value)
+                                            }}
+                                        />
+
+                                    </Form.Group>
+                                </Form>
+                            </Segment> : ""
+                        }
+
+
+
+                        <br />
+                        <br />
+                        <br />
 
                         <Table color='orange' sortable celled>
 
